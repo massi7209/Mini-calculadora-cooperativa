972b9d4306dad6aec7925b730ec219d0e641a0c8
massi7209 <sahoneromassimo@gmail.com>
2025-10-02 10:33:12 -0300

    suma

---
+++ base_calculadora.py
@@ -0,0 +1,17 @@
+def suma(a, b):
+    return a+b
+
+
+def resta(a, b):
+    # TODO: implementar
+    pass
+
+def multiplicar(a, b):
+    # TODO: implementar
+    pass
+
+if __name__ == "__main__":
+    print("Mini Calculadora")
+    print("Suma(2,3):", suma(2,3))
+    print("Resta(5,2):", resta(5,2))
+    print("Multiplicar(3,4):", multiplicar(3,4))
